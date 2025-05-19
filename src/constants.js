09d7c79648af6b69066eec36d6744809d9220a68
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-05-19 16:36:16 +0000

    del: step infos

--- src/constants.js
+++ src/constants.js
@@ -52,29 +52,3 @@ export const addOns = [
     priceYear: "+$20/yr",
   },
 ];
-export const steps = [
-  {
-    id: 1,
-    step: "step 1",
-    title: "your info",
-    linkTo: "/",
-  },
-  {
-    id: 2,
-    step: "step 2",
-    title: "select plan",
-    linkTo: "step2",
-  },
-  {
-    id: 3,
-    step: "step 3",
-    title: "add-ons",
-    linkTo: "step3",
-  },
-  {
-    id: 4,
-    step: "step 4",
-    title: "summary",
-    linkTo: "step4",
-  },
-];
