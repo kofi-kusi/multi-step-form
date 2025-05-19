15f81e5a92bc2a7a191cbd8b9835f67aa70742cc
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-05-19 11:52:39 +0000

    fix: prev-step link style

--- src/components/StepsNavigator.jsx
+++ src/components/StepsNavigator.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function StepsNavigator(props) {
   return (
     <div className="steps--navigator">
-      <div className="">{props.isNotFirstStep && <Link to={props.prevStep}>Go back</Link> }</div>
-      <Link to="/step2">Next Step</Link>
+      {props.isNotFirstStep && <Link className="prev-step" to={props.prevStep}>Go back</Link> }
+      <Link className="next-step" to="/step2">Next Step</Link>
     </div>
   );
 }
