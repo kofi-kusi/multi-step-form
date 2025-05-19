9a6f85a08504507f6fe1df2a401eec1789aff53f
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-05-19 07:13:21 +0000

    fix: changed go back button to use Link

--- src/components/StepsNavigator.jsx
+++ src/components/StepsNavigator.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function StepsNavigator(props) {
   return (
     <div className="steps--navigator">
-      <div className="">{props.isNotFirstStep && <button>Go back</button>}</div>
+      <div className="">{props.isNotFirstStep && <Link to={props.prevStep}>Go back</Link> }</div>
       <Link to="/step2">Next Step</Link>
     </div>
   );
