03ebf316fc81ca7ceae67b925d0661f6a5341ce1
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-05-19 14:54:30 +0000

    feat: pass in step props to each Step component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,14 @@
-import Step from "./Step"
+import Step from "./Step";
 
-function Sidebar() {
-    return (
-        <div className="sidebar">
-            <Step />
-            <Step />
-            <Step />
-            <Step />
-        </div>
-    )
+function Sidebar({ step: activeStep }) {
+  return (
+    <div className="sidebar">
+      <Step step="step1" activeStep={activeStep} number={1} name="STEP 1" info="YOUR INFO" />
+      <Step step="step2" activeStep={activeStep} number={2} name="STEP 2" info="SELECT PLAN" />
+      <Step step="step3" activeStep={activeStep} number={3} name="STEP 3" info="ADD-ONS" />
+      <Step step="step4" activeStep={activeStep} number={4} name="STEP 4" info="SUMMARY" />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
