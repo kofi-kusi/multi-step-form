aff4f1219847b06f21c3c76425d68ff510149b33
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-06-01 14:37:37 +0000

    add: hard coded the ui of step 4

--- src/steps/Step4.jsx
+++ src/steps/Step4.jsx
@@ -1,7 +1,9 @@
 import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Step4() {
-
+    const navigate = useNavigate()
 
     function handleNext(event) {
       event.preventDefault()
@@ -10,7 +12,7 @@ function Step4() {
   
     function handlePrev(event) {
       event.preventDefault()
-      navigate("/step1")
+      navigate("/step3")
     }
 
   return (
@@ -19,6 +21,34 @@ function Step4() {
         title="Finish up"
         instructions="Double-check everything looks OK before confirming."
       />
+      <main>
+        <div className="summary">
+          <div className="summary--plan">
+            <div>
+              <h3>Arcade(Yearly)</h3>
+              <Link to="/step2" className="change-btn">Change</Link>
+            </div>
+            <div>
+              <h3>$9/yr</h3>
+            </div>
+          </div>
+          <hr />
+          <div className="summary--add-ons">
+            <div className="add-ons-summary">
+              <p>Online service</p>
+              <h4>+$10/yr</h4>
+            </div>
+            <div className="add-ons-summary">
+              <p>Larger storage</p>
+              <h4>+$20/yr</h4>
+            </div>
+          </div>
+        </div>
+        <div className="total">
+          <p>Totoal(per monthly)</p>
+          <h2>+$12/mo</h2>
+        </div>
+      </main>
       <div className="steps--navigator">
           <button className="prev-step" onClick={(e) => handlePrev(e)}>Go back</button>
           <button className="next-step" onClick={(e) => handleNext(e)}>Next Step</button>
