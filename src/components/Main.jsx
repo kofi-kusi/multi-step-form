2c82f3c99e10e2d6e8fdd909336bca877b117a75
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-06-03 14:15:36 +0000

    pass state and setters as props to steps

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import Step1 from "../steps/Step1";
 import Step2 from "../steps/Step2";
 import Step3 from "../steps/Step3";
@@ -6,17 +6,29 @@ import Step4 from "../steps/Step4";
 import ErrorPage from "../ErrorPage";
 
 function Main(props) {
-
+  const [storage, setStorage] = useState();
   return (
     <main>
       {props.step === "step2" ? (
-        <Step2/>
+        <Step2
+          setStorage={setStorage}
+          storage={storage}
+        />
       ) : props.step === "step3" ? (
-        <Step3/>
+        <Step3
+          setStorage={setStorage}
+          storage={storage}
+        />
       ) : props.step === "step4" ? (
-        <Step4/>
+        <Step4
+          setStorage={setStorage}
+          storage={storage}
+        />
       ) : props.step === "step1" ? (
-        <Step1  />
+        <Step1  
+          setStorage={setStorage}
+          storage={storage}
+        />
       ) : (
         <ErrorPage />
       )}
